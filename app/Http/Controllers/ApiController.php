c5f76ab9cfc90960308fa2828d24c63d1114cf59
Ibrahim Naeem <naeem@bml.com.mv>
2017-04-02 17:47:15 +0500

    added individual item info

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\RelicSource;
 
 class ApiController extends Controller
 {
@@ -41,5 +42,61 @@ class ApiController extends Controller
             $list[] = $item;
         }
         return $list;
+    }
+
+    public function prime($id)
+    {
+        $i = Item::with('parts.chances.relic.sources.missions')->find($id);
+        if ($i === null) abort(404);
+        $item = [];
+        $item['id'] = $i->id;
+        $item['name'] = $i->name;
+        $item['vaulted'] = ($i->vaulted == 0) ? false : true;
+        foreach ($i->parts as $p) {
+            $part = [];
+            $part['id'] = $p->id;
+            $part['name'] = $p->name;
+            $part['relics'] = [];
+            foreach ($p->chances as $c) {
+                $relic = [];
+                $relic['id'] = $c->relic->id;
+                $relic['name'] = $c->relic->name;
+                $relic['type'] = $c->relic->type;
+                $relic['vaulted'] = $c->relic->vaulted;
+                $relic['intact'] = $c->intact;
+                $relic['exceptional'] = $c->exceptional;
+                $relic['flawless'] = $c->flawless;
+                $relic['radiant'] = $c->radiant;
+                $relic['sources'] = [];
+                foreach ($c->relic->sources as $s) {
+                    $source = [];
+                    $source['id'] = $s->id;
+                    $source['name'] = $s->name;
+                    $source['rotations'] = [];
+                    $ro = RelicSource::with('rotations')->where([
+                                    ['relic_id', '=', $c->relic->id],
+                                    ['source_id', '=', $s->id]])->first();
+                    foreach ($ro->rotations as $r) {
+                        $rotation = [];
+                        $rotation['name'] = $r->name;
+                        $rotation['chance'] = $r->chance;
+                        $source['rotations'][] = $rotation;
+                    }
+                    $source['missions'] = [];
+                    foreach ($s->missions as $m) {
+                        $mission = [];
+                        $mission['name'] = $m->name;
+                        $mission['planet'] = $m->planet;
+                        $mission['faction'] = $m->faction;
+                        $mission['type'] = $m->type;
+                        $source['missions'][] = $mission;
+                    }
+                    $relic['sources'][] = $source;
+                }
+                $part['relics'][] = $relic;
+            }
+            $item['parts'][] = $part;
+        }
+        return $item;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
 
 $app->get('/', 'ApiController@all');
+$app->get('/prime/{id:[0-9]+}', 'ApiController@prime');
 $app->get('/update', 'DataController@update');
